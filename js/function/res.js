84521484af59319a345692b9bab6d94d99cd0b8b
TomasRivetta <totorivetta@gmail.com>
2023-12-29 02:52:35 -0300

    agregando funcionalidad para desplegar las respuestas

--- js/function/res.js
+++ js/function/res.js
@@ -1,8 +1,8 @@
 const responses = document.querySelectorAll("#res")
 
-const icons = document.querySelectorAll("img")
+const iconsPlus = document.querySelectorAll("#plus")
+const iconsMinor = document.querySelectorAll("#minor")
 
-console.log(icons);
 
 export function resToFaq(valor) {
 
@@ -12,11 +12,32 @@ export function resToFaq(valor) {
 
         res = elementRes.dataset.res
 
+
         if (res === valor) {
 
-            responses[res].classList.remove("res")
             responses[res].classList.add("show")
+            iconsPlus[res].classList.add("notIcon")
+            iconsMinor[res].classList.add("showIcon")
 
         }
+
     })
+
+
 }
+
+// if(iconsPlus[res].classList.contains("notIcon")){
+                
+//     iconsPlus[res].classList.remove("notIcon")
+//     iconsPlus[res].classList.add("showIcon")
+//     iconsPlus[res].classList.add("faq-icon-plus")
+
+
+//     iconsMinor[res].classList.remove("showIcon")
+//     iconsPlus[res].classList.add("notIcon")
+
+    
+//     responses[res].classList.add("ocultar")
+//     responses[res].classList.add("res")
+//     responses[res].classList.remove("show")
+// }
